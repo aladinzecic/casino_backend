b022e0be2709c84add973b5c752e6c75743dcf07
aladinzecic <aladinzecic10@gmail.com>
2025-01-15 18:09:01 +0100

    p

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ dotenv.config();
 
 const app = express()
 app.use(cors({
-    origin: 'https://soft-wisp-a67fc0.netlify.app', // Dozvoljava samo ovaj domen
+    origin: ['https://dare2win.netlify.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }));
-  app.use(express.json())
+  
+
+app.use(express.json())
 app.use('/auth',authRouter)
 
 app.listen(process.env.PORT,()=>{
