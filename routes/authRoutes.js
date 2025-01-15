c6b2eb569f1101ce214c0ae40b7d8895333203fa
aladinzecic <aladinzecic10@gmail.com>
2025-01-15 17:14:42 +0100

    push

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,12 +8,12 @@ router.post('/register', async (req,res)=>{
     try{
       const now=Date.now()
         const db= await connectToDatabase()
-        const [rows]= await db.query('SELECT * FROM user WHERE email = ?',[email])
+        const [rows]= await db.query('SELECT * FROM users WHERE email = ?',[email])
         if(rows.length>0){
-            return res.status(409).json({message:"User with this email already exists!"})
+            return res.status(409).json({message:"userswith this email already exists!"})
         }
-        await db.query('INSERT INTO user (username,password,email,createdAt,isActive) VALUES (?,?,?,?,?)',[username,password,email,now,1])
-        res.status(201).json({message:"User succesfuly created!"})
+        await db.query('INSERT INTO users (username,password,email,createdAt,isActive) VALUES (?,?,?,?,?)',[username,password,email,now,1])
+        res.status(201).json({message:"userssuccesfuly created!"})
     }   
     catch(err){
         res.status(500).json(err)
@@ -26,9 +26,9 @@ router.post('/login', async (req,res)=>{
     try {
         const db = await connectToDatabase();
 
-        const [rows] = await db.query('SELECT * FROM user WHERE email = ?', [email]);
+        const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
         if (rows.length === 0) {
-            return res.status(404).json({ message: "User with this email doesn't exist!" });
+            return res.status(404).json({ message: "users with this email doesn't exist!" });
         }
 
         const user = rows[0];
@@ -52,18 +52,18 @@ router.get('/user', async (req, res) => {
         const db = await connectToDatabase();
 
         // Upit za pronalaženje korisnika na osnovu userId
-        const [rows] = await db.query('SELECT  username, razred,caracterNum,caracterEvolLevel,dayStreak,totalXp,numOfCookies,dailySpinsLeft,mathCurr,englishCurr,infoCurr,geoCurr FROM user WHERE id = ?', [userId]);
+        const [rows] = await db.query('SELECT  username, razred,caracterNum,caracterEvolLevel,dayStreak,totalXp,numOfCookies,dailySpinsLeft,mathCurr,englishCurr,infoCurr,geoCurr FROM users WHERE id = ?', [userId]);
 
         // Ako korisnik nije pronađen, vraćamo 404
         if (rows.length === 0) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: 'users not found' });
         }
 
         // Ako je korisnik pronađen, vraćamo njegove podatke
         res.json(rows[0]);
     } catch (err) {
         // Ako dođe do greške u bazi, vraćamo 500
-        res.status(500).json({ error: 'Error retrieving user data', details: err });
+        res.status(500).json({ error: 'Error retrieving users data', details: err });
     }
 });
 router.get('/getPage', async (req, res) => {
@@ -73,11 +73,11 @@ router.get('/getPage', async (req, res) => {
         const db = await connectToDatabase();
 
         // Upit za pronalaženje korisnika na osnovu userId
-        const [rows] = await db.query('SELECT  rouletteVisited, plinkoVisited,huntVisited,minesVisited FROM user WHERE id = ?', [userId]);
+        const [rows] = await db.query('SELECT  rouletteVisited, plinkoVisited,huntVisited,minesVisited FROM users WHERE id = ?', [userId]);
         
         // Ako korisnik nije pronađen, vraćamo 404
         if (rows.length === 0) {
-          return res.status(404).json({ message: 'User not found' });
+          return res.status(404).json({ message: 'users not found' });
         }
         if(page==="PLINKO"){
           res.json(rows[0].plinkoVisited);
@@ -94,7 +94,7 @@ router.get('/getPage', async (req, res) => {
         // Ako je korisnik pronađen, vraćamo njegove podatke
     } catch (err) {
         // Ako dođe do greške u bazi, vraćamo 500
-        res.status(500).json({ error: 'Error retrieving user data', details: err });
+        res.status(500).json({ error: 'Error retrieving users data', details: err });
     }
 });
 
@@ -107,7 +107,7 @@ router.post('/updateMoney', async (req, res) => {
   
       // Ažuriraj oba svojstva u bazi
       await db.query(
-        'UPDATE user SET money = ? WHERE id = ?',
+        'UPDATE users SET money = ? WHERE id = ?',
         [money, userId]
       );
   
@@ -127,25 +127,25 @@ router.post('/updatePage', async (req, res) => {
       // Ažuriraj oba svojstva u bazi
       if(page==="ROULLETE"){
         await db.query(
-          'UPDATE user SET rouletteVisited = ? WHERE id = ?',
+          'UPDATE users SET rouletteVisited = ? WHERE id = ?',
           [true, userId]
         );
       }
       else if(page==="PLINKO"){
         await db.query(
-          'UPDATE user SET plinkoVisited = ? WHERE id = ?',
+          'UPDATE users SET plinkoVisited = ? WHERE id = ?',
           [true, userId]
         );
       }
       else if(page==="HUNT"){
         await db.query(
-          'UPDATE user SET huntVisited = ? WHERE id = ?',
+          'UPDATE users SET huntVisited = ? WHERE id = ?',
           [true, userId]
         );
       }
       else if(page==="MINES"){
         await db.query(
-          'UPDATE user SET minesVisited = ? WHERE id = ?',
+          'UPDATE users SET minesVisited = ? WHERE id = ?',
           [true, userId]
         );
       }
@@ -164,12 +164,12 @@ router.post('/updatePage', async (req, res) => {
       const db = await connectToDatabase();
   
       const [rows] = await db.query(
-        'SELECT money FROM user WHERE id = ?',
+        'SELECT money FROM users WHERE id = ?',
         [userId]
       );
   
       if (rows.length === 0) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: 'users not found' });
       }
   
       const money = rows[0].money;
@@ -177,7 +177,7 @@ router.post('/updatePage', async (req, res) => {
       res.status(200).json({ money });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: 'Error fetching user money' });
+      res.status(500).json({ message: 'Error fetching users money' });
     }
   });
 
@@ -188,12 +188,12 @@ router.post('/updatePage', async (req, res) => {
       const db = await connectToDatabase();
   
       const [rows] = await db.query(
-        'SELECT * FROM user WHERE id = ?',
+        'SELECT * FROM users WHERE id = ?',
         [userId]
       );
   
       if (rows.length === 0) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: 'users not found' });
       }
   
       const all = rows[0];
@@ -201,7 +201,7 @@ router.post('/updatePage', async (req, res) => {
       res.status(200).json({ all });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: 'Error fetching user money' });
+      res.status(500).json({ message: 'Error fetching users money' });
     }
   });
 
@@ -213,12 +213,12 @@ router.post('/updatePage', async (req, res) => {
       const db = await connectToDatabase();
   
       const [rows] = await db.query(
-        'SELECT id FROM user WHERE email = ?',
+        'SELECT id FROM users WHERE email = ?',
         [email]
       );
   
       if (rows.length === 0) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: 'users not found' });
       }
   
       const id = rows[0].id;
@@ -226,7 +226,7 @@ router.post('/updatePage', async (req, res) => {
       res.status(200).json({ id });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: 'Error fetching user id' });
+      res.status(500).json({ message: 'Error fetching users id' });
     }
   });
   
@@ -239,11 +239,11 @@ router.post('/updatePage', async (req, res) => {
     // SQL upit za broj korisnika
     const numOfUsers = await db.query('SELECT COUNT(*) AS count FROM user');
     const newUsers = await db.query(
-      'SELECT COUNT(*) AS dailyUsers FROM user WHERE DATE(FROM_UNIXTIME(createdAt / 1000)) = CURDATE()'
+      'SELECT COUNT(*) AS dailyUsers FROM users WHERE DATE(FROM_UNIXTIME(createdAt / 1000)) = CURDATE()'
     );
     
     const activeUsers=await db.query(
-      'SELECT COUNT(*) AS activeUsers FROM user WHERE lastActiveAt > UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 3 MINUTE)) * 1000;'
+      'SELECT COUNT(*) AS activeUsers FROM users WHERE lastActiveAt > UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 3 MINUTE)) * 1000;'
     )
     const deposited=await db.query(
 1    )
@@ -262,7 +262,7 @@ router.post('/updatePage', async (req, res) => {
     res.status(200).json({ totalUsers: length, newUsers:newUsersNum, activeUsers:activeUsersNum });
   } catch (e) {
     // Greška na serveru
-    res.status(500).json({ message: 'Error fetching user count', error: e.message });
+    res.status(500).json({ message: 'Error fetching users count', error: e.message });
   }
 });
 
@@ -274,7 +274,7 @@ router.post('/updateActivity', async (req, res) => {
 
   try {
     const [result] = await db.query(
-      'UPDATE user SET lastActiveAt = ? WHERE id = ?',
+      'UPDATE users SET lastActiveAt = ? WHERE id = ?',
       [Date.now(), userId]
     );
 
@@ -292,7 +292,7 @@ router.post('/deposit', async (req, res) => {
 
   try {
     const [result] = await db.query(
-      'UPDATE user SET depositedMoney = ? WHERE id = ?',
+      'UPDATE users SET depositedMoney = ? WHERE id = ?',
       [depositedMoney, userId]
     );
 
